13d11554c6b76f1d01abe12c3cfddfefd64aa533
kandabarau <37620848+kandabarau@users.noreply.github.com>
2020-11-17 15:31:13 +0100

    Update filterFragments.sh
    store fragments in separate deirectory

--- scripts/filterFragments.sh
+++ scripts/filterFragments.sh
@@ -8,13 +8,15 @@ dp=$3 # minimal number of reads supporting the fragment
 
 sm=$(samtools view -H ${bm} | grep '^@RG' | sed 's/.*SM://' | cut -f1 | uniq)
 
+mkdir -p fragments/
+
 samtools view ${bm} | grep -v -e 'XA:Z:' -e 'SA:Z:' > ${sm}.uniq.aln
-cut -f3,4,8,9 ${sm}.uniq.aln | sort | uniq -c | sort -nr | grep -v '*' | sed -e 's/ chr/\tchr/g' -e 's/ //g' | awk -v dp=${dp} '$1 >= dp' > ${sm}.seq.fragments
-awk 'FNR==NR{a[$1,$2]; next} ($2,$3) in a' ${fr} ${sm}.seq.fragments | cut -f2,3,4,5 > ${sm}.trg.fragments
-awk 'FNR==NR{a[$1,$2]; next} ($2,$4) in a' ${fr} ${sm}.seq.fragments | cut -f2,3,4,5 >> ${sm}.trg.fragments
+cut -f3,4,8,9 ${sm}.uniq.aln | sort | uniq -c | sort -nr | grep -v '*' | sed -e 's/ chr/\tchr/g' -e 's/ //g' | awk -v dp=${dp} '$1 >= dp' > fragments/${sm}.seq.fragments
+awk 'FNR==NR{a[$1,$2]; next} ($2,$3) in a' ${fr} fragments/${sm}.seq.fragments | cut -f2,3,4,5 > fragments/${sm}.trg.fragments
+awk 'FNR==NR{a[$1,$2]; next} ($2,$4) in a' ${fr} fragments/${sm}.seq.fragments | cut -f2,3,4,5 >> fragments/${sm}.trg.fragments
 
 samtools view -H ${bm} > ${sm}.filter.sam
-awk 'FNR==NR{a[$1,$2,$3,$4]; next} ($3,$4,$8,$9) in a' ${sm}.trg.fragments ${sm}.uniq.aln >> ${sm}.filter.sam
+awk 'FNR==NR{a[$1,$2,$3,$4]; next} ($3,$4,$8,$9) in a' fragments/${sm}.trg.fragments ${sm}.uniq.aln >> ${sm}.filter.sam
 samtools view -hb ${sm}.filter.sam | samtools sort -o ${sm}.filter.bam -
 samtools index ${sm}.filter.bam
 
