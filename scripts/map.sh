d1d1138f3c9914d6bfa0f8278aeabc6d90ced017
kandabarau <kandabarau@gmail.com>
2021-01-11 10:35:39 +0100

    remove control sructure from map.sh; will keep it in script that calls map.sh

--- scripts/map.sh
+++ scripts/map.sh
@@ -2,20 +2,17 @@
 
 # map to the reference of choice
 
-f1=$1
-f2=$2
-sm=$3
-lb=${sm}
-REF=$4
-ct=ct
+f1=$1 # read 1
+f2=$2 # read 2
+sm=$3 # sample
+lb=${sm} # library, specify if differs from sample. not in our case
+REF=$4 # reference fasta
+ct=ct # sequening center, specify here
 
 fc=$(zcat ${f1} | head -1 | cut -d':' -f3)
 ln=$(zcat ${f2} | head -1 | cut -d':' -f4)
 
-if [ ! -e "${sm}_bwamem.bam" ]
-then
 bwa mem -v 1 -t 16 -Y -R "@RG\tID:${fc}.${ln}\tSM:${sm}\tLB:${sm}\tPL:illumina\tPU:${fc}.${lb}.${ln}\tCN:${ct}" ${REF} ${f1} ${f2} > ${sm}_bwamem.bam
-fi
 
 samtools sort -o ${sm}.srt.bam ${sm}_bwamem.bam
 samtools index ${sm}.srt.bam ${sm}.srt.bam.bai
