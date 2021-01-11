e463e4f957fad1a7904f2fc649e2b80db194ee39
kandabarau <kandabarau@gmail.com>
2021-01-11 11:10:49 +0100

    added bam directory for map.sh

--- scripts/map.sh
+++ scripts/map.sh
@@ -8,11 +8,12 @@ sm=$3 # sample
 lb=${sm} # library, specify if differs from sample. not in our case
 REF=$4 # reference fasta
 ct=ct # sequening center, specify here
+bmdir=$5 # dir to write the alignments
 
 fc=$(zcat ${f1} | head -1 | cut -d':' -f3)
 ln=$(zcat ${f2} | head -1 | cut -d':' -f4)
 
-bwa mem -v 1 -t 16 -Y -R "@RG\tID:${fc}.${ln}\tSM:${sm}\tLB:${sm}\tPL:illumina\tPU:${fc}.${lb}.${ln}\tCN:${ct}" ${REF} ${f1} ${f2} > ${sm}_bwamem.bam
+bwa mem -v 1 -t 16 -Y -R "@RG\tID:${fc}.${ln}\tSM:${sm}\tLB:${sm}\tPL:illumina\tPU:${fc}.${lb}.${ln}\tCN:${ct}" ${REF} ${f1} ${f2} > ${bmdir}${sm}_bwamem.bam
 
-samtools sort -o ${sm}.srt.bam ${sm}_bwamem.bam
-samtools index ${sm}.srt.bam ${sm}.srt.bam.bai
+samtools sort -o ${bmdir}${sm}.srt.bam ${bmdir}${sm}_bwamem.bam
+samtools index ${bmdir}${sm}.srt.bam ${bmdir}${sm}.srt.bam.bai
